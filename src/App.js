763d1af29238d32911009fb9c735e57f247a9142
koraykoksal <kksl.koray@gmail.com>
2023-07-31 21:28:40 +0300

    created route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Register from "./pages/Register";
 import Navbar from "./components/Navbar";
+import AppRouter from "./router/AppRouter";
 
 const App = () => {
   return (
-    <div>
-      <Navbar/>
-      <Register />
+    <div className="dark:gray-dark-main min-h-screen">
+      
+
+      <AppRouter/>
+
     </div>
   );
 };
