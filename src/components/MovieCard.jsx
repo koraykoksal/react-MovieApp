e891c0e18724f97119b76b3cddad5cdc07bb95c8
koraykoksal <kksl.koray@gmail.com>
2023-08-03 19:26:41 +0300

    movieCard components

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AutContext } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
-export const MovieCard = () => {
-  return (
-    <div>MovieCard</div>
+export const MovieCard = ({ poster_path, title, overview, vote_average, id }) => {
+  
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+const defaultImage =
+      "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
+    
+const {currentUser}=useContext(AutContext)
+let navigate=useNavigate()
+
+const getVoteClass = (vote) => {
+    if (vote >= 8) {
+      return "green";
+    } else if (vote >= 6) {
+      return "orange";
+    } else {
+      return "red";
+    }
+  };
+  
+return (
+
+    <div
+        className="movie hover:scale-95 hover:rounded-xl"
+        id="container"
+        onClick={() => navigate("/details/" + id)}>
+                <img
+                    loading="lazy"
+                    src={poster_path ? IMG_API + poster_path : defaultImage}
+                    alt="movie-card"
+                />
+        <div className="flex align-baseline justify-between p-1 text-white">
+            <h5>{title}</h5>
+            {currentUser && (
+            <span className={`tag ${getVoteClass(vote_average)}`}>
+            {vote_average.toFixed(1)}
+            </span>
+            )}
+        </div>
+        <div className="movie-over">
+            <h2>Overview</h2>
+            <p>{overview}</p>
+        </div>
+    </div>
   )
 }
